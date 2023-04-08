c83bb3f36c34b69654e35166ee978f017d2c751e
jose <jose.alvarez9212@gmail.com>
2023-04-08 16:11:27 -0600

    change name IAppViewService

--- MisGastos.Prism/MisGastos.Prism/Services/AppView/IDisplayActionService.cs
+++ MisGastos.Prism/MisGastos.Prism/Services/AppView/IDisplayActionService.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace MisGastos.Prism.Services.AppView
 {
-	public interface IDisplayActionService
+	public interface IAppViewService
 	{
        Task DisplayAlert(string title, string message, string cancel);
 
